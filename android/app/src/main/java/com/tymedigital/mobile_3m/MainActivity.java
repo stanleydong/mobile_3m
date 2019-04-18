ba6977c7affadf7d88ca846d1241cfff4d3628f0
CuongDong <cuong.dong@hubcba.com>
2019-04-18 13:31:36 +0700

    Initialize project

---
+++ android/app/src/main/java/com/tymedigital/mobile_3m/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tymedigital.mobile_3m;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
